7fa8ecad2a46be379a60ca0a9ca73fb9d609d801
Mohammed <mohaliyet@gmail.com>
2022-04-14 23:33:16 +0300

    two extra project files are fixed.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,28 @@
 #include "main.h"
+
 /**
- * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
+ * print_number - print an integer (with no trailing newline)
+ * @n: integer to print
+ *
+ * Return: void
  */
 void print_number(int n)
 {
+	unsigned int m;
 
-	return (0);
+	if (n >= 0)
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		m = -1 * n;
+		if (m >= 10)
+			print_number(m / 10);
+		_putchar('0' + m % 10);
+	}
+
 }
